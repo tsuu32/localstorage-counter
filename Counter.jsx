889c0e1b57fe12719be530aae827e77224396349
Masahiro Nakamura <13937915+tsuu32@users.noreply.github.com>
2020-01-29 12:09:49 +0900

    Increase the size of clicker button.

--- Counter.jsx
+++ Counter.jsx
@@ -1,8 +1,9 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import { Button } from "@blueprintjs/core";
+import styled from "styled-components";
 
 export default function Counter(props) {
-  const [count, setCount] = useState(Number(localStorage.getItem("count")));
+  const [count, setCount] = useState(parseInt(localStorage.getItem("count"), 10));
   const incCount = useCallback(() => setCount((cnt) => cnt+1), [setCount]);
   const cmHandler = useCallback((ev) => {
     if (props.noContextMenu)
@@ -17,6 +18,18 @@ export default function Counter(props) {
   });
 
   return (
-    <Button intent="primary" large={true} onClick={incCount} onContextMenu={cmHandler}>{count}</Button>
+    <StyledButton
+      intent="primary"
+      large={true}
+      onClick={incCount}
+      onContextMenu={cmHandler}
+    >
+      {count}
+    </StyledButton>
   );
 }
+
+const StyledButton = styled(Button)`
+  width: 280px;
+  height: 280px;
+`;
--- index.jsx
+++ index.jsx
@@ -5,32 +5,40 @@ import "@blueprintjs/core/lib/css/blueprint.css";
 
 import React, { useState, useEffect, useCallback } from 'react';
 import ReactDOM from 'react-dom';
+import styled from "styled-components";
 import Header from './header';
 import Counter from './counter';
 
 function App() {
   const [noContextMenu, setNoContextMenu] = useState(false);
   const [cheetRightClick, setCheetRightClick] = useState(false);
-  const toggleNoContextMenu = useCallback(() => setNoContextMenu((prev) => !prev),
-                                          [setNoContextMenu]);
-  const toggleCheetRightClick = useCallback(() => setCheetRightClick((prev) => !prev),
-                                            [setCheetRightClick]);
+  const toggleNoContextMenu = useCallback(() => {
+    setNoContextMenu((prev) => !prev);
+  }, [setNoContextMenu]);
+  const toggleCheetRightClick = useCallback(() => {
+    setCheetRightClick((prev) => !prev);
+  }, [setCheetRightClick]);
 
   return (
-    <div>
-      <Header noContextMenu={noContextMenu} cheetRightClick={cheetRightClick}
-              toggleNoContextMenu={toggleNoContextMenu} toggleCheetRightClick={toggleCheetRightClick}/>
+    <>
+      <Header
+        noContextMenu={noContextMenu}
+        cheetRightClick={cheetRightClick}
+        toggleNoContextMenu={toggleNoContextMenu}
+        toggleCheetRightClick={toggleCheetRightClick}
+      />
       <Container>
-        <Counter noContextMenu={noContextMenu} cheetRightClick={cheetRightClick}/>
+        <Counter
+          noContextMenu={noContextMenu}
+          cheetRightClick={cheetRightClick}
+        />
       </Container>
-    </div>
+    </>
   );
 }
 
-import styled from "styled-components";
-
 const Container = styled.div`
-  padding: 2rem;
+  padding: 2rem 0;
   text-align: center;
   touch-action: manipulation;
 `;
